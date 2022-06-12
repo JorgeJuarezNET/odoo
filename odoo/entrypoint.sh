3e408b2ad708f8282b704eaeed833401d2060ae4
JorgeJuarezNET <104749639+JorgeJuarezNET@users.noreply.github.com>
2022-06-12 18:17:06 -0500

    [ADD] Migrations (#1)
    * [ADD] Migrations
    
    * Fix: db cache parameter

--- odoo/entrypoint.sh
+++ odoo/entrypoint.sh
@@ -8,10 +8,11 @@ fi
 
 # set the postgres database host, port, user and password according to the environment
 # and pass them as arguments to the odoo process if not present in the config file
-: ${HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
-: ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
-: ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
-: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
+: ${ODOO_DB_HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
+: ${ODOO_DB_PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
+: ${ODOO_DB_USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
+: ${ODOO_DB_PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
+: ${ODOO_SERVER_WIDE_MODULES:='web'}
 
 DB_ARGS=()
 function check_config() {
@@ -26,20 +27,46 @@ function check_config() {
 
 
 echo "============= SED ==================="
-sed -i -e "s/db_host =.*/db_host = $HOST/" "$ODOO_RC"
-sed -i -e "s/db_user =.*/db_user = $USER/" "$ODOO_RC"
-sed -i -e "s/db_password =.*/db_password = $PASSWORD/" "$ODOO_RC"
+sed -i -e "s/db_host =.*/db_host = $ODOO_DB_HOST/" "$ODOO_RC"
+sed -i -e "s/db_user =.*/db_user = $ODOO_DB_USER/" "$ODOO_RC"
+sed -i -e "s/db_password =.*/db_password = $ODOO_DB_PASSWORD/" "$ODOO_RC"
+sed -i -e "s/; server_wide_modules =.*/server_wide_modules = $ODOO_SERVER_WIDE_MODULES/" "$ODOO_RC"
 
 
-check_config "db_host" "$HOST"
-check_config "db_port" "$PORT"
-check_config "db_user" "$USER"
-check_config "db_password" "$PASSWORD"
+check_config "db_host" "$ODOO_DB_HOST"
+check_config "db_port" "$ODOO_DB_PORT"
+check_config "db_user" "$ODOO_DB_USER"
+check_config "db_password" "$ODOO_DB_PASSWORD"
 
 
-export PGUSER=$USER
-export PGPASSWORD=$PASSWORD
-export PGHOST=$HOST
+export PGUSER=$ODOO_DB_USER
+export PGPASSWORD=$ODOO_DB_PASSWORD
+export PGHOST=$ODOO_DB_HOST
+export PGPORT=$ODOO_DB_PORT
+
+export MARABUNTA_DATABASE=demo
+export MARABUNTA_DB_USER=$ODOO_DB_USER
+export MARABUNTA_DB_PASSWORD=$ODOO_DB_PASSWORD
+export MARABUNTA_DB_HOST=$ODOO_DB_HOST
+export MARABUNTA_MODE=base
+
+export DEFAULTDB=postgres
+
+function drop() {
+    echo "$PGHOST"
+    echo "Dropping $1"
+    dropdb --if-exists --maintenance-db=$DEFAULTDB $1
+    echo "Database $1 has been dropped"
+}
+
+function create() {
+  echo "Start create function"
+  EXIST=$(psql -X -A -t $DEFAULTDB -c "SELECT 1 AS result FROM pg_database WHERE datname = '$1'";)
+  if [ "$EXIST" != "1" ]; then
+    echo "Creating $1"
+    createdb --maintenance-db=$DEFAULTDB $1
+  fi
+}
 
 case "$1" in
     -- | odoo)
@@ -47,17 +74,21 @@ case "$1" in
         if [[ "$1" == "scaffold" ]] ; then
             exec odoo "$@"
         else
+            echo "${DB_ARGS[@]}"
             wait-for-psql.py ${DB_ARGS[@]} --timeout=30
-            DB_EXIST=$(psql -X -A -t -d postgres -c "SELECT 1 AS result FROM pg_database WHERE datname = 'demo'";)
-            if [ "$DB_EXIST" = "1" ]; then
-                python3 -m click_odoo -d demo /script.py
-            fi
-            exec odoo "$@" "${DB_ARGS[@]}"
+            echo "DB: Ready"
+            drop demo
+            create demo
+            sed -i -e "s/; db_name =.*/db_name = $MARABUNTA_DATABASE/" "$ODOO_RC"
+            gosu odoo marabunta -f /migration.yml
+            sed -i -e "s/db_name =.*/; db_name = $MARABUNTA_DATABASE/" "$ODOO_RC"
+            echo "Migration Finished"
+            exec gosu odoo odoo "$@" "${DB_ARGS[@]}"
         fi
         ;;
     -*)
         wait-for-psql.py ${DB_ARGS[@]} --timeout=30
-        exec odoo "$@" "${DB_ARGS[@]}"
+        exec gosu odoo odoo "$@" "${DB_ARGS[@]}"
         ;;
     *)
         exec "$@"
---
+++ odoo/songs/setup.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2022 Jorge Luis Juarez Mandujano
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+import anthem
+
+
+@anthem.log
+def setup_attachmnts_db(ctx):
+    """Set Attachments DB Rules"""
+    ctx.env["ir.config_parameter"].set_param(
+        "ir_attachment.storage.force.database",
+        "{\"image/\": 51200, \"application/javascript\": 0, \"text/css\": 0}"
+    )
+
+    ctx.env["ir.config_parameter"].set_param(
+        "ir_attachment.location",
+        "db"
+    )
+
+    ctx.env['ir.attachment'].force_storage()
+
+
+@anthem.log
+def main(ctx):
+    """Setup Attachents"""
+    setup_attachmnts_db(ctx)
